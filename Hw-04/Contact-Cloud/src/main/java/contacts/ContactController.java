2a51a8b41e04c8d7df5cb23e6be2a756086df9a9
sizhang_ding <sizhang_ding@126.com>
2023-10-06 18:00:33 +0800

    Hw-04 added

--- Hw-04/Contact-Cloud/src/main/java/contacts/ContactController.java
+++ Hw-04/Contact-Cloud/src/main/java/contacts/ContactController.java
@@ -1,4 +1,42 @@
 package contacts;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.*;
+
+@Controller
+@RequestMapping("/")
+
 public class ContactController {
+    //TODO
+
+    private List<Contact> contacts = new ArrayList<>();
+
+    @ModelAttribute(name = "contact")
+    public Contact contact() {
+        return new Contact();
+    }
+
+    @ModelAttribute(name = "contacts")
+    public List<Contact> contacts() {
+        return this.contacts;
+    }
+
+    @GetMapping
+    public String showContactsForm() {
+        return "home";
+    }
+
+    @PostMapping
+    public String processContacts(
+            @Valid Contact contact, Errors errors) {
+        if(!errors.hasErrors()) {
+            contacts.add(contact);
+        }
+        return "home";
+    }
 }
